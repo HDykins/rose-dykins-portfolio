2b037c0eab1946b8eeb77a66cd91423ef5b0c466
Harry Dykins <HarryDykins@Harrys-MacBook-Pro.local>
2016-06-04 16:25:08 +0100

    Add slick carousel and implement basic usage. Add building blocks of website structure

--- build/js/app.js
+++ build/js/app.js
@@ -1,5 +1,30 @@
 var container = document.getElementById('container');
+var pdfView = false;
+
+function togglePdfView() {
+	if (pdfView) {
+		pdfView = false;
+	} else {
+		pdfView = true;
+	}
+}
+
+function getPdfView() {
+	return pdfView;
+}
 
 $('body').on('click', '#container', function (clickEvent) {
-	new PdfViewer({pdfUrl: 'Junior developer CV_short', staticHost: 'https://dl.dropboxusercontent.com/u/46887483/Junior%20developer%20CV_short.pdf'}).embed(container);
+	if (getPdfView()) {
+		$('#container').empty();
+	} else {
+		new PdfViewer({pdfUrl: 'Junior developer CV_short', staticHost: 'https://dl.dropboxusercontent.com/u/46887483/Junior%20developer%20CV_short.pdf'}).embed(container);
+	}
+	togglePdfView();
+	console.log(pdfView);
+});
+
+$('.slick').slick({
+  centerMode: true,
+  centerPadding: '60px',
+  slidesToShow: 3
 });
