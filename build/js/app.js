e069b01af1bfc5781d96648662f232c11077f2de
Harry Dykins <HarryDykins@Harrys-MacBook-Pro.local>
2016-06-05 18:53:18 +0100

    Improve slick-carousel and add styling

--- build/js/app.js
+++ build/js/app.js
@@ -13,18 +13,39 @@ function getPdfView() {
 	return pdfView;
 }
 
-$('body').on('click', '#container', function (clickEvent) {
-	if (getPdfView()) {
-		$('#container').empty();
-	} else {
+$('body').on('click', '.slick-slide', function (clickEvent) {
+	if (!getPdfView()) {
+		$('.main').hide();
+		$('#container').show();
 		new PdfViewer({pdfUrl: 'Junior developer CV_short', staticHost: 'https://dl.dropboxusercontent.com/u/46887483/Junior%20developer%20CV_short.pdf'}).embed(container);
 	}
 	togglePdfView();
 	console.log(pdfView);
 });
 
-$('.slick').slick({
+$('body').on('click', '#container', function (clickEvent) {
+	if (getPdfView()) {
+		$('#container').empty().hide();
+		$('.main').show();
+	}
+});
+
+$('#container').hide();
+
+ $('.slider-for').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  fade: true,
+  asNavFor: '.slider-nav',
+  adaptiveHeight: true
+});
+
+$('.slider-nav').slick({
   centerMode: true,
-  centerPadding: '60px',
-  slidesToShow: 3
+  slidesToShow: 3,
+  speed: 200,
+  asNavFor: '.slider-for',
+  dots: true,
+  adaptiveHeight: true
 });
