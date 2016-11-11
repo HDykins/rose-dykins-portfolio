2fff88690cde50363460200aa3b3cc6fdfec7cac
Harry Dykins <HarryDykins@Harrys-MacBook-Pro.local>
2016-11-11 17:42:35 +0000

    Add possible fix for images not loading

--- source/js/app.jsx
+++ source/js/app.jsx
@@ -1,5 +1,7 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
-var Application = require('./components/Application.jsx');
+$(window).load(function() {
+	var React = require('react');
+	var ReactDOM = require('react-dom');
+	var Application = require('./components/Application.jsx');
 
-ReactDOM.render(<Application />, document.querySelector('[data-rose-portfolio-react-application]'));
+	ReactDOM.render(<Application />, document.querySelector('[data-rose-portfolio-react-application]'));
+});
